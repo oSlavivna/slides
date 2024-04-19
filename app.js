e79781d7d177120349901332c531dd3f9f88248a
Slavivna <hotyninfo@gmail.com>
2024-04-19 12:03:42 +0300

    open close on click

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
 // todo: add logic
+// let slides = document.querySelectorAll(".slide");
+
+// slides.forEach(function (slide) {
+//   slide.addEventListener("click", function () {
+//     let isActive = this.classList.contains("active");
+
+//     if (!isActive) {
+//       let activeSlide = document.querySelector(".slide.active");
+//       if (activeSlide) {
+//         activeSlide.classList.remove("active");
+//       }
+
+//       this.classList.add("active");
+//     }
+//   });
+// });
+//////////////////////////////
 let slides = document.querySelectorAll(".slide");
+
+let prev = null;
 
 slides.forEach(function (slide) {
   slide.addEventListener("click", function () {
     let isActive = this.classList.contains("active");
 
     if (!isActive) {
-      let activeSlide = document.querySelector(".slide.active");
-      if (activeSlide) {
-        activeSlide.classList.remove("active");
+      if (prev !== null) {
+        prev.classList.remove("active");
       }
-
       this.classList.add("active");
+      prev = this;
+    } else {
+      this.classList.remove("active");
+      prev = null;
     }
   });
 });
