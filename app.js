4dd12e1c99fd5c6aafdcf212697cf4abd4a1e9a9
Slavivna <hotyninfo@gmail.com>
2024-04-19 12:26:45 +0300

    v1

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
 // todo: add logic
-// let slides = document.querySelectorAll(".slide");
-
-// slides.forEach(function (slide) {
-//   slide.addEventListener("click", function () {
-//     let isActive = this.classList.contains("active");
-
-//     if (!isActive) {
-//       let activeSlide = document.querySelector(".slide.active");
-//       if (activeSlide) {
-//         activeSlide.classList.remove("active");
-//       }
-
-//       this.classList.add("active");
-//     }
-//   });
-// });
-//////////////////////////////
 let slides = document.querySelectorAll(".slide");
-
-let prev = null;
 
 slides.forEach(function (slide) {
   slide.addEventListener("click", function () {
     let isActive = this.classList.contains("active");
 
     if (!isActive) {
-      if (prev !== null) {
-        prev.classList.remove("active");
+      let activeSlide = document.querySelector(".slide.active");
+      if (activeSlide) {
+        activeSlide.classList.remove("active");
       }
+
       this.classList.add("active");
-      prev = this;
-    } else {
-      this.classList.remove("active");
-      prev = null;
     }
   });
 });
+//////////////////////////
+//щоб по кліку фото закривалось/відкривалось
+//але чомусь Презединт не закривається прикліку на наступне фото, якщо його спеціально не закрити ⬇️
+// let prev = null;
+
+// slides.forEach(function (slide) {
+//   slide.addEventListener("click", function () {
+//     let isActive = this.classList.contains("active");
+
+//     if (!isActive) {
+//       if (prev !== null) {
+//         prev.classList.remove("active");
+//       }
+//       this.classList.add("active");
+//       prev = this;
+//     } else {
+//       this.classList.remove("active");
+//       prev = null;
+//     }
+//   });
+// });
